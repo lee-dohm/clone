094955166b50897ff486af463c931549d379196b
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2017-12-30 07:10:41 -0800

    :arrow_up: version_tasks@0.10.29

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule Clone.Mixfile do
   defp deps do
     [
       {:ex_doc, "> 0.0.0", only: :dev, runtime: false},
+      {:version_tasks, "~> 0.10", only: :dev},
       {:espec, "~> 1.5", only: :test}
     ]
   end
