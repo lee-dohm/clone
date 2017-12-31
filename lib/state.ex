5ae780c2338ccd88058d497f22aebb3bfa58234f
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2017-12-31 09:57:06 -0800

    :memo: Flesh out documentation of Clone.State

--- lib/state.ex
+++ lib/state.ex
@@ -1,17 +1,36 @@
 defmodule Clone.State do
   @moduledoc """
-  Represents the state of the application.
+  Represents the state of things as the program executes.
+
+  This structure collects the important information that may be of interest to multiple functions
+  as the program executes. It starts with the parsed command-line options and arguments provided
+  by `OptionParser`. From there, other information that is needed for the program to do its work
+  are added to be passed on to later functions.
+
+  For another example of the same pattern used at larger scale, see [`Plug.Conn`][plug-conn].
+
+  [plug-conn]: https://hexdocs.pm/plug/Plug.Conn.html
   """
   defstruct [:arguments, :invalid_options, :location, :options, :repo_dir]
 
-  @type t :: %__MODULE__{}
+  @type t :: %__MODULE__{
+    arguments: [String.t],
+    invalid_options: Keyword.t,
+    location: String.t | nil,
+    options: %{required(atom) => any},
+    repo_dir: String.t | nil
+  }
+
   @type parsed_options :: {OptionParser.parsed, OptionParser.argv, OptionParser.errors}
 
   @doc """
   Creates a `Clone.State` struct from an `OptionParser.parse/2` tuple.
+
+  Options are transformed into a `Map` for easier pattern matching. Arguments and invalid options
+  are stored as-is.
   """
   @spec new(parsed_options) :: t
-  def new({options, arguments, invalid}) do
+  def new({options, arguments, invalid} = _parsed_options) do
     %__MODULE__{
       arguments: arguments,
       invalid_options: invalid,
